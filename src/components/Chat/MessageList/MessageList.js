0c58b9f81f9e4e7269b00446df358e4dab1723ce
46655916+jcraggs@users.noreply.github.com <46655916+jcraggs@users.noreply.github.com>
2020-04-22 20:14:36 +0100

    fix: auto scroll of chat works on mobiles now

--- src/components/Chat/MessageList/MessageList.js
+++ src/components/Chat/MessageList/MessageList.js
@@ -5,6 +5,11 @@ import "./MessageList.css";
 import Message from "./Message/Message";
 
 const MessageList = ({ messages, name, chatboxBool }) => {
+  let element = document.querySelector(".css-13azwyo.messages");
+  if (element !== null) {
+    element.scrollTo(9999, 9999);
+  }
+
   return (
     <ScrollToBottom className={chatboxBool === true ? "hide" : "messages"}>
       {messages.map((message, i) => (
